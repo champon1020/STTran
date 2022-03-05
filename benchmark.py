9df0ffe2aa8820635b50af6f753d815b0d00c96b
champon1020 <nagatelu1020@gmail.com>
2022-03-05 13:24:12 +0900

    flops calculation

--- benchmark.py
+++ benchmark.py
@@ -5,12 +5,15 @@ np.set_printoptions(precision=4)
 import copy
 
 import torch
+import torch.nn as nn
 from tqdm import tqdm
 
 from dataloader.action_genome import AG, cuda_collate_fn
 from lib.config import Config
 from lib.object_detector import detector
 from lib.sttran import STTran
+
+from lib.ults.flop_count import flop_count, fmt_res
 
 conf = Config()
 for i in conf.args:
@@ -53,9 +56,22 @@ print("*" * 50)
 print("CKPT {} is loaded".format(conf.model_path))
 
 
+class CombinedModel(nn.Module):
+    def __init__(self, detector, classifier, gt_annotation):
+        super().__init__()
+        self.detector = detector
+        self.classifier = classifier
+        self.gt_annotation = gt_annotation
+
+    def forward(self, entry):
+        entry = self.detector(entry[0], entry[1], entry[2], entry[3], self.gt_annotation, im_all=None)
+        entry = self.classifier(entry)
+        return list(entry.values())
+
 speed_time = 0.0
 speed_time_including_detector = 0.0
 n_iters = 0
+tmp = []
 with torch.no_grad():
     for b, data in enumerate(tqdm(dataloader)):
         im_data = copy.deepcopy(data[0].cuda(0))
@@ -75,9 +91,17 @@ with torch.no_grad():
         pred = model(entry)
         elapsed_time = time.time() - start
 
+        combined_model = CombinedModel(object_detector, model, gt_annotation)
+        inputs = [im_data, im_info, gt_boxes, num_boxes]
+        res = flop_count(combined_model, (inputs,))
+        tmp.append(sum(res.values()))
+
         speed_time += elapsed_time
         speed_time_including_detector += elapsed_time
         n_iters += 1
+
+        if n_iters >= 10:
+            break
 
 speed_time /= n_iters
 speed_time_including_detector /= n_iters
@@ -87,5 +111,6 @@ n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
 n_parameters_inc_detector = n_parameters + sum(p.numel() for p in object_detector.parameters() if p.requires_grad)
 print("Number of params:", n_parameters)
 print("Number of paramas including detector:", n_parameters_inc_detector)
+print("flops", fmt_res(np.array(tmp)))
 print("Speed (s/image): ", speed_time)
 print("Speed including detector (s/image): ", speed_time_including_detector)
